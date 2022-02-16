791104cae7e0fa11f4deb0ec9a4e8b66c070667f
yoshi98da93 <99373920+yoshi98da93@users.noreply.github.com>
2022-02-16 12:20:40 +0900

    0216

--- src/index.js
+++ src/index.js
@@ -19,11 +19,19 @@
 // console.log(arr2);
 
 //配列のコピー、結合
-const arr4 = [10, 20];
-const arr5 = [30, 40];
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
 
-const arr6 = [...arr4];
-console.log(arr6);
+// const arr6 = [...arr4];
+// arr6[0]=100;
+// console.log(arr6);
+// console.log(arr4);
+// console.log(arr6);
 
-const arr7 = [...arr4, ...arr5];
-console.log(arr7);
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+// const arr8=arr4;
+// arr8[0]=100
+// console.log(arr4);
+// console.log(arr8);
