e516252a87f85b6aa2ea349e0bd2db6fee77c31d
yoshi98da93 <99373920+yoshi98da93@users.noreply.github.com>
2022-02-16 12:16:10 +0900

    0216

--- src/index.js
+++ src/index.js
@@ -2,10 +2,28 @@
  * スプレッド構文...
  */
 //配列の展開
-const arr1 = [1, 2];
-// console.log(arr1);
-// console.log(...arr1);
+// const arr1 = [1, 2];
+// // console.log(arr1);
+// // console.log(...arr1);
 
-const sumFunc = (num1, num2) => console.log(num1 + num2);
-sumFunc(arr1[0], arr1[1]);
-sumFunc(...arr1);
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+//まとめる
+// const arr2=[1,2,3,4,5];
+// const[num1,num2,...arr3]=arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+// console.log(arr2);
+
+//配列のコピー、結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+const arr6 = [...arr4];
+console.log(arr6);
+
+const arr7 = [...arr4, ...arr5];
+console.log(arr7);
